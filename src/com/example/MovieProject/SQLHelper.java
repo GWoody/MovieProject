44387a9037532d4704d5c5afca3a83c50bec10e1
GWoody <garyjw93@gmail.com>
2014-10-14 11:14:39 +1100

    added basic DB structure

---
+++ src/com/example/MovieProject/SQLHelper.java
@@ -0,0 +1,30 @@
+package com.example.MovieProject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Gareth on 14/10/2014.
+ */
+public class SQLHelper extends SQLiteOpenHelper {
+
+    public SQLHelper(Context context)
+    {
+        super(context, DatabaseContract.DATABASE_NAME,null,DatabaseContract.DATABASE_VERSION); // set context and DB name
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(DatabaseContract.WatchedTable.CREATE_WATCHED_TABLE); // create the tables
+        db.execSQL(DatabaseContract.ToWatchTable.CREATE_TOWATCH_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
