2b78a5f51137d791f7f0744c666f031758361e85
Aleksander <afrus@bigpond.com>
2014-10-17 20:12:12 +1100

    Activity now setup to hold fragments within child element of Navigation Drawer correctly

--- src/com/example/MovieProject/MyActivity.java
+++ src/com/example/MovieProject/MyActivity.java
@@ -1,6 +1,8 @@
 package com.example.MovieProject;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
@@ -17,14 +19,19 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main); //testing layout files
+
+        Fragment fragment = new HomeFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
         String [] mDrawerTitles = {"Watched", "Watch List", "Search Movies"};
 
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mDrawerList = (ListView)findViewById(R.id.left_drawer);
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerTitles));
-
-
     }
 }
