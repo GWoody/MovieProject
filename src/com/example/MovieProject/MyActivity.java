db805949a645f77ecfb6708236ac033871952fa2
Aleksander <afrus@bigpond.com>
2014-10-17 19:31:10 +1100

    Navigation Drawer now displays, no functionality as yet

--- src/com/example/MovieProject/MyActivity.java
+++ src/com/example/MovieProject/MyActivity.java
@@ -2,8 +2,15 @@ package com.example.MovieProject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MyActivity extends Activity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,13 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main); //testing layout files
+
+        String [] mDrawerTitles = {"Watched", "Watch List", "Search Movies"};
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView)findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerTitles));
+
+
     }
 }
